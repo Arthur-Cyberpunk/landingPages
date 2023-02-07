44458c175d08baf3575228e310859de937a2b1e5
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-02-07 15:24:09 -0300

    initial config

--- src/templates/App/index.js
+++ src/templates/App/index.js
@@ -1,9 +1,12 @@
+import * as Styled from "./styles";
+
 function Home() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Styled.Wrapper background="blue">
+        <h1>Hello</h1>
+      </Styled.Wrapper>
     </div>
   );
 }
-
 export default Home;
