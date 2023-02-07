7d86f47e339b9f2a2db22a8a82fc733bf0d84b11
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-02-07 15:16:42 -0300

    initial config

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import Home from './templates/App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Home />
+  </React.StrictMode>
 );
