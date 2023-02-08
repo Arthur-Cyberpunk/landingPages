76afd4be2d841c52551d2f38b7d7e77587b83b11
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-02-08 15:23:33 -0300

    config tests

--- src/templates/App/App.test.js
+++ src/templates/App/App.test.js
@@ -1,16 +1,8 @@
-import { screen } from "@testing-library/react";
 import Home from ".";
 import { renderTheme } from "../../styles/render-theme";
-import { theme } from "../../styles/theme";
 
-test("renders learn react link", () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer =
-    // eslint-disable-next-line testing-library/no-node-access
-    screen.getByRole("heading", { name: "Hello" }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe("<Home />", () => {
+  it("should render home", () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toMatchSnapshot();
-  expect(headingContainer).toHaveStyleRule("background", "red");
 });
