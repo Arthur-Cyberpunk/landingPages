219294a8793b4f23664cc4c83d0c764f7ec17ad2
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-02-10 15:45:35 -0300

    created grid columns

--- src/components/GridTwoColumn/GridTwoColum.test.jsx
+++ src/components/GridTwoColumn/GridTwoColum.test.jsx
@@ -1,10 +1,11 @@
-import { screen } from '@testing-library/react';
 import { GridTwoColum } from '.';
 import { renderTheme } from '../../styles/render-theme';
 
+import mock from './mock';
+
 describe('<GridTwoColum />', () => {
-  it('should render', () => {
-    renderTheme(<GridTwoColum>Children</GridTwoColum>);
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+  it('should render two column grid', () => {
+    const { container } = renderTheme(<GridTwoColum {...mock} />);
+    expect(container).toMatchSnapshot();
   });
 });
--- src/components/GridTwoColumn/stories.jsx
+++ src/components/GridTwoColumn/stories.jsx
@@ -1,11 +1,13 @@
 import { GridTwoColum } from '.';
 
+import mock from './mock';
+
 export default {
   title: 'GridTwoColum',
   component: GridTwoColum,
   args: {
     title: 'Grid two columns',
-    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio magnam culpa eveniet doloribus harum? Ipsam, a necessitatibus? Sequi sunt accusantium quod, animi iure a, aliquid dolor ea vel magni dolore?`,
+    args: mock,
     srcImg: 'assets/images/javascript.svg',
   },
   argTypes: {
