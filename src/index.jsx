01756b4863b2a48b2912b2a6f06ebc47ff0cfc58
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-02-22 16:25:44 -0300

    correcting the errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 import Home from './templates/Home';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Routes>
-          <Route to="*" element={Home} />
-        </Routes>
+        <Switch>
+          <Route to="*" component={Home} />
+        </Switch>
       </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
